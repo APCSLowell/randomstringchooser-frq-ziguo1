821afa9f87fb9561702d4486e7738592dab97acd
ziguo1 <ziguo1@s.sfusd.edu>
2025-04-24 09:09:17 +0000

    oudhgourehgouergf

--- src/main/java/RandomLetterChooser.java
+++ src/main/java/RandomLetterChooser.java
@@ -4,7 +4,10 @@ public class RandomLetterChooser extends RandomStringChooser
 	public RandomLetterChooser(String str)
 	{ 
 		/* to be implemented in part (b) */
-		
+		super(new String[str.length()]);
+		for (int i = 0; i < str.length(); i++) {
+			strs[i] = str.substring(i, i + 1);
+		}
 	}
 	
 	
--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -5,4 +5,23 @@ public class RandomStringChooser
   
   //Heads up! 
   //You will get a very confusing error message until you have working code in part b as well
+  protected String[] strs;
+  public RandomStringChooser(String[] s) { strs = s; }
+  public String getNext() {
+    if (strs.length == 0) return "NONE";
+    int chosen = (int) (Math.random() * strs.length);
+    String stri = strs[chosen];
+
+    String[] n = new String[strs.length - 1];
+    int ptr = 0;
+    for (int i = 0; i < strs.length; i++) {
+      String get = strs[i];
+      if (get != stri) {
+        n[ptr] = get;
+        ptr++;
+      }
+    }
+    strs = n;
+    return stri;
+  }
 }
